33e5e77aaef997dd23b664d80c4019ae13edc1d5
guinuxbr <guinuxbr@gmail.com>
2023-01-27 23:05:58 +0000

    Re-added the alias to dotfiles command.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,3 +2,6 @@ alias ls='ls --color=auto'
 
 # Custom command to install Grub at default/fallback boot path
 alias grub2-removable='sudo grub2-install --target=x86_64-efi --efi-directory=/boot/efi --removable'
+
+# Custom Git command to work with git@github.com:guinuxbr/.dotfiles.git
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
